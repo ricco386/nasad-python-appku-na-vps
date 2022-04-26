3ed8e866dc56f3a9d0426b01ee2a5be0f5354a8b
Richard Kellner <richard.kellner@gmail.com>
2022-04-26 10:05:09 +0200

    Added ability to load setting from ENV variables

--- meetup/settings.py
+++ meetup/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j2l$tlbx@g1q6^lgga(alnjsfn3f1fm7#j7okuvl%!xmhf!391'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv('ALLOWED_HOST'),
+]
 
 
 # Application definition
